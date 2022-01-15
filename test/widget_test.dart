3555d5cf2c7f41569dacd90348217bd55e4dd248
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-15 18:37:54 +0900

    delete comment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -17,8 +10,12 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 //テスト用Widgetを定義　言語を引数に取るように設定
 Widget myAppTest(String language) {
   return MaterialApp(
+    title: 'Flutter Demo',
     localizationsDelegates: AppLocalizations.localizationsDelegates,
     supportedLocales: AppLocalizations.supportedLocales,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
     //機種の言語設定の代わりになる部分
     locale: Locale(language),
     home: Builder(builder: (context) {
